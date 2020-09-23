25010337cb1c91e66b17dfe6208dc58457078186
admin <damien.jacquemart@dataiku.com>
2020-09-23 12:57:29 +0200

    added key value mapping

--- python-runnables/create-meaning/runnable.py
+++ python-runnables/create-meaning/runnable.py
@@ -1,5 +1,5 @@
 # This file is the actual code for the Python runnable create-meaning
-from dataiku.runnables import Runnable
+from dataiku.runnables import Runnable, ResultTable
 import dataiku
 
 class PluginParamValidationError(ValueError):
@@ -20,6 +20,31 @@ class MyRunnable(Runnable):
         self.config = config
         self.plugin_config = plugin_config
         
+    def get_value_list(self, dataset, column):
+        value_list = list(dataset.get_dataframe(columns=[column])[column].unique())
+        
+        return value_list
+        
+    def get_key_value_mapping(self, dataset):
+        
+        rt = ResultTable()
+        rt.add_column("key", "key", "STRING")
+        rt.add_column("value", "value", "STRING")
+        
+        row_list =[] 
+        for index, rows in dataset.get_dataframe().iterrows(): 
+            # Create list for the current row 
+            my_list =[rows[self.config.get("key")], rows[self.config.get("val")]] 
+
+            # append the list to the final list
+            row_list.append(my_list)
+            rt.add_record(my_list)
+            
+        mappings = [{"from": item[0], "to": item[1]} for item in row_list]
+            
+        return mappings, rt
+
+        
     def get_progress_target(self):
         """
         If the runnable will return some progress info, have this function return a tuple of 
@@ -34,33 +59,71 @@ class MyRunnable(Runnable):
         """
         parameters = self.config
         action = parameters["action"]
+        meaning_type = parameters["meaning_type"]
         dataset_name = parameters['input_dataset_name']
         column = parameters['column_name']
         
         client = dataiku.api_client()
         
         dataset = dataiku.Dataset(dataset_name, self.project_key)
-        value_list = list(dataset.get_dataframe(columns=[column])[column].unique())
                 
         if action == "create_new":
+            
+            print(parameters)
+            
             new_meaning_name = parameters["new_meaning_name"]
             list_existing_meanings = [meaning['id'].lower() for meaning in client.list_meanings()]
             if new_meaning_name.lower() in list_existing_meanings:
                 raise PluginParamValidationError("Meaning name already exists")
 
-            client.create_meaning(new_meaning_name, new_meaning_name, "VALUES_LIST",
-                    values=value_list,
-                    normalizationMode="EXACT"
-            )
+            if meaning_type == "list_of_values":
+                
+                value_list = self.get_value_list(dataset, column)
+                client.create_meaning(new_meaning_name, 
+                                      new_meaning_name, 
+                                      "VALUES_LIST",
+                                      values=value_list,
+                                      normalizationMode="EXACT"
+                                      )
             
-            return "Custom meaning successfully created."
+                return "Custom meaning successfully created."
+            
+            if meaning_type == "key_value_mapping":
+                
+                mappings, rt = self.get_key_value_mapping(dataset)
+                    
+                client.create_meaning(new_meaning_name.replace(" ","_"),
+                                      new_meaning_name,
+                                      "VALUES_MAPPING",
+                                      mappings=mappings
+                                     )
+                
+                return rt
         
         if action == "update":  
-            meaning_to_update = parameters["meaning_name"]
-            meaning = client.get_meaning(meaning_to_update)
-            definition = meaning.get_definition()
-            definition['values'] = value_list
-            meaning.set_definition(definition)
             
-            return "Custom meaning successfully updated."
-                         
+            meaning_to_update = parameters["meaning_name"]
+            
+            if meaning_type == "list_of_values":
+                
+                value_list = self.get_value_list(dataset, column)
+                
+                meaning = client.get_meaning(meaning_to_update)
+                definition = meaning.get_definition()
+                definition['values'] = value_list
+                meaning.set_definition(definition)
+                
+                return "Custom meaning successfully created."
+
+                
+            if meaning_type == "key_value_mapping":
+                                
+                mappings, rt = self.get_key_value_mapping(dataset)
+                
+                meaning = client.get_meaning(meaning_to_update)
+                definition = meaning.get_definition()
+                definition['mappings'] = mappings
+                meaning.set_definition(definition)
+        
+                return rt
+                                    
